c81518f60df53919c08f228a248695dcc6d322b3
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2023-12-02 01:18:25 +0100

    feat: project setup with nestjs & typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from "../entities/user.entity";
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: false,
+  entities: [User],
+  autoLoadEntities: true,
+  migrationsRun: process.env.DATABASE_MIGRATION === 'true',
+  migrationsTableName: 'migration',
+  migrations: [process.env.DATABASE_MIGRATION_FILES ?? 'dist/migration/*.js'],
+  cli: {
+    migrationsDir: 'migration',
+  },
+  logging: process.env.DATABASE_MIGRATION === 'true',
+  logger: 'advanced-console',
+};
+
+export default typeOrmConfig;
