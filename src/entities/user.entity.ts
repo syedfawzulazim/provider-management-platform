d82eac9f6787fa7d52e4a5211215ebb096772df6
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2023-12-03 14:36:54 +0100

    user model and entity created

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,9 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { UserModel } from "../models/user.model";
 
-@Entity()
-export class User {
+@Entity({name: 'user'})
+export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string
 
   @Column()
   firstName: string;
@@ -11,6 +18,38 @@ export class User {
   @Column()
   lastName: string;
 
-  @Column({ default: true })
-  isActive: boolean;
+  @Column({ default: false })
+  isSuperAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+static fromModel(model: UserModel): UserEntity{
+    const entity = new UserEntity();
+    entity.id = model.id;
+    entity.email = model.email;
+    entity.password = model.password;
+    entity.firstName = model.firstName;
+    entity.lastName = model.lastName;
+    entity.isSuperAdmin = model.isSuperAdmin;
+    entity.createdAt = model.createdAt;
+    entity.updatedAt = model.updatedAt;
+    return entity;
+  }
+
+  toModel(): UserModel {
+    return new UserModel({
+      id: this.id,
+      email: this.email,
+      password: this.password,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      isSuperAdmin: this.isSuperAdmin,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    })
+  }
 }
