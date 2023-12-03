c6e13ab7b9c44c2fbc89c4ccaa0b151555cf7215
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2023-12-03 14:35:03 +0100

    update: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from "./auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import typeOrmConfig from "./config/typeorm.config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
