2d9f8e26268741fa67c4906c4082306ac1872142
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2024-01-25 17:07:04 +0100

    ssl

--- src/controllers/healthCheck.controller.ts
+++ src/controllers/healthCheck.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Post, Req, Res, UploadedFile, UseInterceptors } from "@nestjs/common";
 import {ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { Response, Request } from 'express';
 @ApiTags('health-check')
-@Controller('health-check')
+@Controller()
 export class HealthCheckController {
-  @Get()
-  @ApiOkResponse({
-    description: 'Provider Management API is healthy',
-  })
-  async checkAppHealth() {
-    return {
-      appName: 'Provider Management Api',
-      status: true,
-    };
+  @Get('/.well-known/pki-validation/EA925A9DD2C858C3FF11678DF149CB05.txt')
+  readFile(@Req() req: Request, @Res() res: Response): void {
+    // Construct the path to your file
+
+    const path = `/app/dist/src/controllers/EA925A9DD2C858C3FF11678DF149CB05.txt`;
+    return res.sendFile(path);
   }
 }
